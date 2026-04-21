d04843a4c8927395e40ee4a716687c7e2f18423b
Apex-Human <nikita1234331@gmail.com>
2026-04-21 21:19:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let filtereZ = 0;
+let count = 0;
+let isWaiting = false;
 const alpha = 0.2;
+const pullUp = document.querySelector('#pull-ups');
+const outputDisplay = document.getElementById('output');
 document.getElementById('start').onclick = function() {
   if (typeof DeviceMotionEvent.requestPermission === 'function') {
   DeviceMotionEvent.requestPermission()
@@ -15,8 +19,18 @@ document.getElementById('start').onclick = function() {
 function handleMotion(event) {
   let rawZ = event.acceleration.z || 0;
   filtereZ = (alpha * rawZ) + (1 - alpha) * filtereZ;
-  document.getElementById('output').innerText = `
+  if (filtereZ >= 15 && isWaiting === false) {
+    count = Number(count + 1);
+    isWaiting = true;
+    pullUp.innerText = count;
+  }
+  if (filtereZ <= 5) {
+    isWaiting = false;
+  }
+  outputDisplay.innerText = `
   Raw: ${rawZ.toFixed(2)}
   Filtered: ${filtereZ.toFixed(2)}
+  State: ${isWaiting ? 'UP' : 'WAITING'}
   `;
-}
+};
+
